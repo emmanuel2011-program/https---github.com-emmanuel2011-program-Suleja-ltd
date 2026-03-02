d1eeb8b5f6d55fb16a1dc16c6dce8a14401788ec
Emmanuel <okoyee61@gmail.com>
2026-03-02 15:05:59 +0100

    added update to membership

--- app/dashboard/membership/page.tsx
+++ app/dashboard/membership/page.tsx
@@ -2,6 +2,9 @@ import { fetchAllMembers } from '@/app/lib/data';
 import { lusitana } from '@/app/ui/fonts';
 import MemberTableWrapper from '@/app/ui/memberships/table-wrapper';
 
+// This line is the fix! It forces Vercel to fetch fresh data on every request.
+export const dynamic = 'force-dynamic';
+
 export default async function Page() {
   const members = await fetchAllMembers();
 
